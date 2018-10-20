22cb43a46833d28738da8091f45feab0e456e643
chimerablack1 <chimerablaque@hotmail.com>
2018-10-20 11:51:14 -0400

    3-12 done, JWT (token) added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://christof123:christof123@ds149353.mlab.com:49353/devconnector'
-}
+  mongoURI: 'mongodb://christof123:christof123@ds149353.mlab.com:49353/devconnector',
+  secretOrKey: 'secret'
+};
