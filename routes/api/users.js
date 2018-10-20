000df8d6b88579aebbd6a8803d14203c83b9b26e
chimerablack1 <chimerablaque@hotmail.com>
2018-10-20 11:03:27 -0400

    Added in User Registration route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,31 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
-
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: "Users Works"}));
-
+router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
 // @route   GET api/users/register
 // @desc    Register user
 // @access  Public
-router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email })
-  .then(user => {
-    if(user) {
-      return res.status(400).json({email: 'Email already exists'});
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: '200', //size
-        r: 'pg', //rating
-        d: 'mm' //default
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
       });
 
       const newUser = new User({
@@ -36,12 +33,13 @@ router.post('/register', (req, res) => {
       });
 
       bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) =>{
-          if(err) throw err;
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
           newUser.password = hash;
-          newUser.save()
+          newUser
+            .save()
             .then(user => res.json(user))
-            .catch(err => console.log(err)); 
+            .catch(err => console.log(err));
         });
       });
     }
