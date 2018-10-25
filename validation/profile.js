2dc12961daac0cb8c2af113c0feb1910839abef7
chimerablack1 <chimerablaque@hotmail.com>
2018-10-25 13:03:17 -0400

    added profile.js file to validation folder

---
+++ validation/profile.js
@@ -0,0 +1,27 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateProfileInput(data) {
+    let errors = {};
+
+    data.handle = !isEmpty(data.handle) ? data.handle : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+
+    if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid";
+    }
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required";
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "password field is required";
+    }
+
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+};
