2304c5bb9b15d74d133835d9203bba9852434052
chimerablack1 <chimerablaque@hotmail.com>
2018-11-24 16:28:11 -0500

    made it so you can register a user with the sign up form, and it will create a user in mlab.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
@@ -24,6 +24,12 @@ class Register extends Component {
         this.setState({ [e.target.name]: e.target.value });
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors })
+        }
+    }
+
     onSubmit(e) {
         e.preventDefault();
 
@@ -35,10 +41,8 @@ class Register extends Component {
         }
 
 
-        this.props.registerUser(newUser);
-        // axios.post('/api/users/register', newUser)
-        //     .then(res => console.log(res.data))
-        //     .catch(err => this.setState({ errors: err.response.data }));
+        this.props.registerUser(newUser, this.props.history);
+
     }
 
 
@@ -46,11 +50,9 @@ class Register extends Component {
 
         const { errors } = this.state;
 
-        const { user } = this.props.auth;
-
         return (
             <div className="register">
-                {user ? user.name : null}
+
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 m-auto">
@@ -122,12 +124,14 @@ class Register extends Component {
 
 Register.propTypes = {
     registerUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => ({
-    auth: state.auth
+    auth: state.auth,
+    errors: state.errors,
 })
 
 
-export default connect(mapStateToProps, { registerUser })(Register);
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
